5fe5f94d4d94a4e0ae6c31c46e137b931561f13b
Iurii Kurinnoi <yur4kur@icloud.com>
2023-11-20 15:40:23 +0500

    Initial Commit

---
+++ Lesson-4.02_ColorizedApp/Lesson_4_02_ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_02_ColorizedAppApp.swift
+//  Lesson-4.02_ColorizedApp
+//
+//  Created by Юрий Куринной on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_02_ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
